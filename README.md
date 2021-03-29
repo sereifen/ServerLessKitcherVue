70e7f936be6b2a4a040699a3e387dd7583064b29
joan <joan.calmetmarin@afry.com>
2021-03-29 15:22:23 +0200

    set functional

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# vue-s5ju6h
+# serverlesskitchen
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-s5ju6h)
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
-import { createWebHistory, createRouter } from "vue-router"
-import Home from "./components/Home"
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Home',
+    component: Home
   },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
+
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
